e16a950b423d7d2bf4551c1771e6e7122e9a7b8c
mazhaoyou <953866349@qq.com>
2018-11-29 22:43:50 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const DB = {
+    host: "188.131.174.180",
+    port: 3306,
+    user: "root",
+    password: "mzy123",
+    database: "blog"
+};
+
+module.exports = DB;
---
+++ database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const config = require('./config');
+
+const database = mysql.createConnection({
+    host:config.host,
+    user:config.user,
+    port:config.port,
+    password:config.password,
+    database:config.database,
+    multipleStatements: true //允许执行多条语句
+
+});
+
+database.connect();
+
+module.exports = database;
