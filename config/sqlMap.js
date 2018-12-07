c8e19b99b422b95db76943d863ad193bb3eeea63
mazhaoyou <953866349@qq.com>
2018-12-07 19:06:53 +0800

    新增更新语句

--- config/sqlMap.js
+++ config/sqlMap.js
@@ -4,6 +4,7 @@ var sqlMap = {
         select_name: 'SELECT * from users where name = ?',    //查询 name
         select_number:'SELECT * from users where number = ?', //查询number是否存在
         select_password: 'SELECT * from users where password = ?',   //查询 password是否正确
+        update_password:'UPDATE users set password = ? where number = ?',
     }
 }
 module.exports = sqlMap;
